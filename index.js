d7ccfed78c4c19278773c0d9638dcff2f5461b93
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-08 10:46:00 -0600

    update to index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('dotenv');
 const express = require('express');
 const server = express()
 
-console.log(process.env.USERDOMAIN);
-
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
